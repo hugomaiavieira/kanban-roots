21a601a1b3a6d25c0f84cb6fcbf5b7644ab217d0
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-09-14 19:08:10 -0300

    Add mysql database

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KanbanRoots::Application.config.secret_token = 'e8927debe4b6b9af34c2cc6347bcff2e975f562bcc62285d3408b20f905f46caa2059ce98fe914a1311846f5ab82c56dd709d8adb0f539800679177989d43743'
+KanbanRoots::Application.config.secret_token = '70745b925167d41f7b45bb0a8d7abe9e7eafc7f5443d6d6cf9bb4411ff0581a4d9da970fb5a718cb3fd1e05515898a9658b1b1da37e5db122b4f4d6e0bdc4b9f'
