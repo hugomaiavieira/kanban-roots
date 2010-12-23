87ce5504fcc3bc7dc83b02c5ecfcf49406bb21b4
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-12-23 11:46:10 -0200

    Tiny refactoring

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,10 @@
 class Board
+
+  BACKLOG = 'Backlog'
+  TODO = 'To Do'
+  DOING = 'Doing'
+  DONE = 'Done'
+  POSITIONS = [BACKLOG, TODO, DOING, DONE]
+
 end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-  # XXX: The positions should be on Board Model
-  POSITIONS = ['Backlog', 'To Do', 'Doing', 'Done']
+
   CATEGORIES = ['Bug', 'Feature']
   POINTS = [0, 1, 2, 3, 5, 8, 13]
 
