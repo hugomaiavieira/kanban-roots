e98aff85a23c6a6bdd4be1ce643fa0d847a14d55
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-01-06 13:24:12 -0200

    Add 'Refactoring' to Task's categories

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :project
   has_and_belongs_to_many :contributors
 
-  CATEGORIES = ['Bug', 'Feature']
+  CATEGORIES = ['Bug', 'Feature', 'Refactoring']
   POINTS = [0, 1, 2, 3, 5, 8, 13]
 
   validates_presence_of :title, :project
