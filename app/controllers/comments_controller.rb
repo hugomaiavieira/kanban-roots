16f423a291f7989c735958f29e89c6a0d2533df4
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-01-13 10:28:49 -0200

    Remove all 'Back' links; Add a project board shortcut

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,14 @@ class CommentsController < InheritedResources::Base
 
   belongs_to :task
 
+  def edit
+    edit! { @project = @task.project }
+  end
+
+  def new
+    new! { @project = @task.project }
+  end
+
   def create
     create! { redirect_to(project_task_path(@comment.task.project, @comment.task)) and return }
   end
