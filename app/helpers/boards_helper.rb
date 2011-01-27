94931d02c25387fa936a89347ae9d7cb4816fdec
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-01-27 14:31:57 -0200

    Removed lost |

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,24 +1,40 @@
 module BoardsHelper
 
   def to_postit task
-    category_class = task.category.nil? ? '' : " #{task.category.downcase}"
 
-    "<div class='postit#{category_class}'>
-      <p class='points'>
-        #{if task.points.nil?
-          link_to 'Set points', edit_project_task_path(task.project, task)
-        else
-          task.points
-        end}
+    "<div class='postit#{category_class(task)}'>
+      <p class='top'>
+        #{points(task)}
+        #{comments(task)}
       </p>
-      #{link_to task.title, project_task_path(task.project, task), :class => :title}
+      #{title(task)}
       #{sponsors(task)}
     </div>"
+  end
 
+  def category_class task
+    category_class = task.category.nil? ? '' : " #{task.category.downcase}"
+  end
+
+  def points task
+    if task.points.nil?
+      link_to 'Set points', edit_project_task_path(task.project, task)
+  else
+      task.points
+    end
+  end
+
+  def comments task
+    "<span class ='comments_number'>#{task.comments.count} comments</span>"
+  end
+
+  def title task
+    link_to truncate(task.title, :length => 45),
+      project_task_path(task.project, task),
+      :class => :title
   end
 
   def sponsors task
-
     if task.contributors.empty?
       return "<p class='sponsor'>
         #{link_to 'Set sponsor', edit_project_task_path(task.project, task)}
@@ -36,7 +52,6 @@ module BoardsHelper
     return "<#{p}>
       #{truncate(sponsors_sentence, :length => 25)}
     </p>"
-
   end
 
 end
