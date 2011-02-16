d350b6ede849aee78f3a701b8534f3e001741fab
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-02-16 11:40:49 -0200

    Tiny refactoring for textarea and Markdown note

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,10 @@ module ApplicationHelper
 
   def markdown_note(item)
     "<span class='markdown_note'>
-       Note: #{item} are rendered using Markdown, a lightweight markup language.
+       Note: #{item} are rendered using
+       <span class='help_cursor' title='A lightweight markup language'>
+        Markdown
+       </span>.
        See
        <a href='http://en.wikipedia.org/wiki/Markdown#Syntax_examples'>
          this examples</a> and
