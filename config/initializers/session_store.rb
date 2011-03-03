3e6078433ff1a9b1042151ff4343890543db77ac
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-03-02 23:33:05 -0300

    Run 'rake rails:update'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ KanbanRoots::Application.config.session_store :cookie_store, :key => '_kanban_ro
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # KanbanRoots::Application.config.session_store :active_record_store
