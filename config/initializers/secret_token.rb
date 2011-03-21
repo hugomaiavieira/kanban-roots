c59e54ee967d702a3cd29e958bf621d57795db6f
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-03-21 10:51:12 -0300

    Changed secret_token for Devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KanbanRoots::Application.config.secret_token = '70745b925167d41f7b45bb0a8d7abe9e7eafc7f5443d6d6cf9bb4411ff0581a4d9da970fb5a718cb3fd1e05515898a9658b1b1da37e5db122b4f4d6e0bdc4b9f'
+KanbanRoots::Application.config.secret_token = '70845b925167d41f7b45bb0a8d7abe9e7eafc7f5443d6d6cf9bb4411ff0581a4d9da970fb5a718cb3fd1e05515898a9658b1b1da37e5db122b4f4d6e0bdc4b9f'
+
