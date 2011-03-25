5cf03ec65995a3724ea156caa560ca26d47c14a8
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-03-25 13:00:50 -0300

    Uncoment YAML::ENGINE

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 # couldn't parse YAML at line 7 column 14 (Psych::SyntaxError)
 # RubyGems 1.5.0 seems to force use of the psych yaml parser if you have it in
 # your system. This make ruby use syck instead of psych to parse yaml.
-#require 'yaml'
-#YAML::ENGINE.yamler= 'syck'
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
 
 # Initialize the rails application
 KanbanRoots::Application.initialize!
