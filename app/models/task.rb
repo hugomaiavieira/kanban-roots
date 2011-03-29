e2afcf5a095c1bed55dd18e9ddf1a578b2e4eb7f
Eduardo Hertz <eduardohertz@gmail.com>
2011-03-29 09:42:06 -0300

    Secure task delete. issue #30

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :category
   has_and_belongs_to_many :contributors
-  has_many :comments
+  has_many :comments, :dependent => :delete_all
 
   POINTS = [0, 1, 2, 3, 5, 8, 13]
 
