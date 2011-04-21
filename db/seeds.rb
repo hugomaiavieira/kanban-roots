493b0d2b18ca8d884f18229c2ca457ed24856d06
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-04-21 17:04:31 -0300

    Add some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Project.delete_all
+Contributor.delete_all
+
+Contributor.create!(
+  :name => 'John Doe',
+  :email => 'contributor@example.com',
+  :password => '123456',
+  :password_confirmation => '123456'
+)
+
+Project.create!(
+  :name => 'kanban-roots',
+  :description => 'A kanban board that keeps the simplicity as well as the roots of the concept.',
+  :owner_id => Contributor.all.first.id
+)
+
