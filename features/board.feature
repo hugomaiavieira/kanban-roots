3480bdb61a11689f2bb9d64d1d5a38123716cb12
Rodrigo Manhães <rmanhaes@gmail.com>
2011-04-24 04:00:09 -0300

    added feature: drag and drop tasks from a board position to another

--- features/board.feature
+++ features/board.feature
@@ -28,3 +28,16 @@ Feature: Use the board
     When I am on the projects board page
     Then I should see "Legend: None Bug Feature Refactoring"
 
+
+  @javascript
+  Scenario: Drag and drop a task to another board position
+    Given I am an authenticated contributor
+    And I have a project
+    And the following tasks:
+      | title  | position |
+      | task 1 | Doing    |
+      | task 2 | Done     |
+    And I am on the projects board page
+    When I drag "task 1" task to "Done" position
+    Then I should see "task 1" task at "Done" position
+
--- features/step_definitions/board_steps.rb
+++ features/step_definitions/board_steps.rb
@@ -1,8 +1,20 @@
+When /^I drag "([^"]*)" task to "([^"]*)" position$/ do |task_title, position|
+  task = Task.find_by_title(task_title)
+  postit = find_by_id(task.id)
+  target_position = find_by_id(position.downcase)
+  postit.drag_to(target_position)
+end
+
 Then /^I should see a board like this:$/ do |table|
   table.diff!(tableish('table tr', 'td,th'))
 end
 
 Then /^the Done division should be cleaned$/ do
   @project.tasks_by_position(Board::POSITIONS['done']).should be_empty
+end
+
+Then /^I should see "([^"]*)" task at "([^"]*)" position$/ do |task_title, position|
+  task = Task.find_by_title(task_title)
+  page.should have_xpath "//ul[@id='#{position.downcase}']/li[@id='#{task.id}']"
 end
 
