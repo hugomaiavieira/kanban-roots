4b3f3ba9dc27be6d4d72667b81134d7f27f0123d
Gabriel L. Oliveira <ciberglo@gmail.com>
2011-10-06 17:31:09 -0300

    let the divisions be droppable, accepting the post-its from others divisions, dynamically

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -19,58 +19,17 @@ $(function() {
     revert: 'invalid'
   });
 
-  // TODO: Make this work and remove the others methods. The $(this).attr('id')
-  //       do not return what I whant. Yeah, I don't know javascript. Shame on me.
-//  // let the divisions be droppable, accepting the post-its from others divisions
-//  $('.droppable').droppable({
-//    accept: $accepted_by[$(this).attr('id')],
-//		hoverClass: 'ui-state-hover',
-//    drop: function(event, ui) {
-//      movePostit(ui.draggable, $(this));
-//      defineHeight();
-//    }
-//  });
-
-  // let the todo be droppable, accepting the doing, done and backlog items
-  $todo.droppable({
-    accept: $accepted_by['todo'],
-		hoverClass: 'ui-state-hover',
-    drop: function(event, ui) {
-      movePostit(ui.draggable, $(this));
-      defineHeight();
-    }
+  // let the divisions be droppable, accepting the post-its from others divisions
+  $('.droppable').each(function(key, value){
+      $(value).droppable({
+        accept: $accepted_by[value.id],
+        hoverClass: 'ui-state-hover',
+        drop: function(event, ui) {
+          movePostit(ui.draggable, $(value));
+          defineHeight();
+        }
+      });
   });
-
-  // let the doing be droppable, accepting the todo, done and backlog items
-  $doing.droppable({
-    accept: $accepted_by['doing'],
-		hoverClass: 'ui-state-hover',
-    drop: function(event, ui) {
-      movePostit(ui.draggable, $(this));
-      defineHeight();
-    }
-  });
-
-  // let the done be droppable, accepting the todo, doing and backlog items
-  $done.droppable({
-    accept: $accepted_by['done'],
-		hoverClass: 'ui-state-hover',
-    drop: function(event, ui) {
-      movePostit(ui.draggable, $(this));
-      defineHeight();
-    }
-  });
-
-  // let the backlog be droppable, accepting the todo, doing and done items
-  $backlog.droppable({
-    accept: $accepted_by['backlog'],
-		hoverClass: 'ui-state-hover',
-    drop: function(event, ui) {
-      movePostit(ui.draggable, $(this));
-      defineHeight();
-    }
-  });
-
 });
 
 // slide points select
